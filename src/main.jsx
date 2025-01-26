d007708bfff004141c5c1910e56d13af6796cceb
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-01-26 21:12:51 +0530

    Project Refactoring & Restructuring

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./styles/styles.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./components/App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  
+  <React.StrictMode>
+    
     <App />
-  </StrictMode>,
-)
+    
+  </React.StrictMode>
+);
