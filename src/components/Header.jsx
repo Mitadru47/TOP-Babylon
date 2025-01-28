8f03af0043cc1c863b045643e9223ef9a45fae1d
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-01-28 19:39:12 +0530

    Preliminary Header Concept Established

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,19 @@ function Header(){
 
         <div id="header-component" className="component">
             
-            <div>Header</div>
+            <div id="site-title-container">
+
+                <div id="site-title">Babylon</div>
+
+            </div>
             
+            <div id="greeting-date-time-container">
+
+                <div id="greeting">Hi User!</div>
+                <div id="header-date-time">Tue Jan 28 2025 19:24:51 GMT+0530 (India Standard Time)</div>
+
+            </div>
+
         </div>
     );
 }
