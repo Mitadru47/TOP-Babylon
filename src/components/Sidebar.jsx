7cb49f41a402bb6efaeff1114f4a6f960a5be621
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-01-29 14:21:08 +0530

    Preliminary Sidebar Concept Established

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,54 @@ function Sidebar(){
 
         <div id="sidebar-component" className="component">
             
-            <div>Sidebar</div>
+            <div id="top-info-container">
+
+                <div id="posts-count">Total Posts: 2</div>
+                <div id="users-count">Total Users: 2</div>
+
+                <hr></hr>
+
+            </div>
+
+            <div id="bottom-user-buttons-container">
+
+                <div id="user-buttons-group1">
+
+                    <div id="my-profile-button-container">
+
+                        <a href="">My Profile</a>
+
+                    </div>
+
+                    <div id="my-posts-button-container">
+
+                        <a href="">My Posts</a>
+
+                    </div>
+
+                    <div id="my-comments-button-container">
+
+                        <a href="">My Comments</a>
+
+                    </div>
+
+                    <br></br>
+
+                    <div id="archived-posts-button-container">
+
+                        <a href="">Archived Posts</a>
+
+                    </div>
+
+                </div>
+                
+                <div id="user-buttons-group2">
+
+                    <a href="">Log out</a>
+
+                </div>
+
+            </div>
             
         </div>
     );
