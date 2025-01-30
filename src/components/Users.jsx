9c310d45a5f5b629b077b56764b3bdd4e39125fc
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-01-30 20:55:18 +0530

    Preliminary User-List Concept Established

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,23 @@
 function Users(){
+
     return(
 
         <div id="users-component" className="component">
             
-            <div>Users</div>
+            <div id="users-list-header-container">
+            
+                <div id="users-list-header">Fellow Babylonians</div>
+                
+            </div>
+
+            <div id="users-list-container">
+                
+                <div className="users-list">Ned</div>
+                <div className="users-list">Sweetrobin</div>
+                <div className="users-list">LordOfCasterlyRock</div>
+                <div className="users-list">MaceTheAce</div>
+
+            </div>
             
         </div>
     );
