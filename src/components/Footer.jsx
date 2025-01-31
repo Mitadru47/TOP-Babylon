f60de914cc6dd25ebe7edc72a2b83aaaf313bf77
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-01-31 19:31:46 +0530

    Preliminary Footer Concept Established

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,28 @@ function Footer(){
 
         <div id="footer-component" className="component">
             
-            <div>Footer</div>
+            <div id="footer-top-row">
+
+                <div id="footer-country">India</div>
+                
+                <div id="footer-links-container">
+
+                    <div id="footer-link1"><a href="">Project Idea</a></div>
+                    <div id="footer-link2"><a href="">Server Repository</a></div>
+                    <div id="footer-link3"><a href="">Application Repository</a></div>
+                    <div id="footer-link4"><a href="">Author Profile</a></div>
+
+                </div>
+
+            </div>
+
+            <div id="footer-bottom-row">
+
+                <div id="footer-copyright-info">Iterative Improv &copy; 2025</div>
+
+                <div id="footer-message">Created with ❤️ by Mitadru Gangopadhyay</div>
+
+            </div>
             
         </div>
     );
