4a100c1c0f40678f01880a9da0cbbb12e9f2fc01
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-02-01 13:06:36 +0530

    Preliminary Content-Card Concept Established

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,34 @@
-function Content(){
+function Content(props){
     return(
 
-        <div id="content-component" className="component">
+        <div className="content-card">
             
-            <div>Content</div>
+            <div className="content-card-top">
+
+              <div className="post-author">{props.author}</div>
+              <div className="post-date">{props.date}</div>
+
+            </div>
+
+            <div className="content-card-middle">
+
+                <div className="post-title">{props.title}</div>
+                <div className="post-body">{props.body}</div>
+
+            </div>
+
+
+            <div className="content-card-bottom">
+
+                <div className="post-buttons">
+
+                    <div className="post-button1"><a href="">View</a></div>
+                    <div className="post-button1"><a href="">Comment</a></div>
+                    <div className="post-button1"><a href="">Archive</a></div>
+
+                </div>
+
+            </div>
             
         </div>
     );
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -20,7 +20,49 @@ function Feed(){
 
                 <Sidebar />
                 
-                <Content />
+                <div id="content-card-container">
+
+                    <Content 
+                    
+                        author="Ned" 
+                        date="Tue Jan 28 2025 19:24:51 GMT+0530 (India Standard Time)"
+
+                        title="Eddard To Arya Stark"
+                        body="When the snows fall and the white winds blow, the lone wolf dies, but the pack survives."
+
+                    />
+
+                    <Content 
+                    
+                        author="LordOfCasterlyRock" 
+                        date="Tue Jan 28 2025 19:24:51 GMT+0530 (India Standard Time)"
+
+                        title="Tywin To Tyrion Lannister"
+                        body="The greatest fools are ofttimes more clever than the men who laugh at them."
+                                
+                    />
+
+                    {/* <Content 
+                    
+                        author="MaceTheAce" 
+                        date="Tue Jan 28 2025 19:24:51 GMT+0530 (India Standard Time)"
+
+                        title="Mace to Joffrey I Baratheon"
+                        body="Is there anything as pointless as a king without a kingdom? No, it's plain, the boy must abandon the riverlands, join his forces to Roose Bolton's once more, and throw all his strength against Moat Cailin. That is what I would do."
+                                
+                    />
+
+                    <Content 
+                    
+                        author="Ned" 
+                        date="Tue Jan 28 2025 19:24:51 GMT+0530 (India Standard Time)"
+
+                        title="Eddard To Robb Stark"
+                        body="Know the men who follow you and let them know you. Don't ask your men to die for a stranger."
+
+                    /> */}
+
+                </div>
                 
                 <Users />
 
