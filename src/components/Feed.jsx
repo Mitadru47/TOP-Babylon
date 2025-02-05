424f6f343391a24a1f2dc0298bf3ab94e7f763f4
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-02-05 13:09:47 +0530

    Feed Component - Axios Implementation

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,80 +8,116 @@ import Users from "./Users.jsx";
 
 import Content from "./Content.jsx";
 
+import axios from "axios";
+import { BABYLON_SERVER_URL } from "../utils/urls";
+
+import { useEffect, useState } from "react";
+
+async function getPosts(setPosts){
+
+    axios.get(BABYLON_SERVER_URL + "/posts")
+
+        .then((response) => setPosts(response.data))
+        .catch((error) => console.log(error));
+}
+
 function Feed(){
-    return(
 
-        <div id="feed-page" className="page">
-        
-            <div className="page-top page-section">
-                <Header />
+    const [posts, setPosts] = useState();
+
+    useEffect(() => {
+        getPosts(setPosts);
+
+    }, []);
+
+    if(posts){
+
+        return(
+
+            <div id="feed-page" className="page">
             
-            </div>
-
-            <div className="page-middle page-section">
-
-                <div className="page-left-bottom page-section">
-
-                    <Sidebar />
-                    <LeftFooter />
-
-                </div>
+                <div className="page-top page-section">
+                    <Header />
                 
-                <div id="content-card-container">
-
-                    <Content 
-                    
-                        author="Ned" 
-                        date="Tue Jan 28 2025 19:24:51 GMT+0530 (India Standard Time)"
-
-                        title="Eddard To Arya Stark"
-                        body="When the snows fall and the white winds blow, the lone wolf dies, but the pack survives."
-
-                    />
-
-                    <Content 
-                    
-                        author="LordOfCasterlyRock" 
-                        date="Tue Jan 28 2025 19:24:51 GMT+0530 (India Standard Time)"
-
-                        title="Tywin To Tyrion Lannister"
-                        body="The greatest fools are ofttimes more clever than the men who laugh at them."
-                                
-                    />
-
-                        <Content 
-                    
-                        author="MaceTheAce" 
-                        date="Tue Jan 28 2025 19:24:51 GMT+0530 (India Standard Time)"
-
-                        title="Mace to Joffrey I Baratheon"
-                        body="Is there anything as pointless as a king without a kingdom? No, it's plain, the boy must abandon the riverlands, join his forces to Roose Bolton's once more, and throw all his strength against Moat Cailin. That is what I would do."
-                                
-                    />
-
-                    <Content 
-                    
-                        author="Ned" 
-                        date="Tue Jan 28 2025 19:24:51 GMT+0530 (India Standard Time)"
-
-                        title="Eddard To Robb Stark"
-                        body="Know the men who follow you and let them know you. Don't ask your men to die for a stranger."
-
-                    />
-
                 </div>
-                
-                <div className="page-right-bottom page-section">
 
-                    <Users />                   
-                    <RightFooter />
+                <div className="page-middle page-section">
+
+                    <div className="page-left-bottom page-section">
+
+                        <Sidebar />
+                        <LeftFooter />
+
+                    </div>
+                    
+                    <div id="content-card-container">
+
+                        {posts.map((post, index) => {
+                            
+                            return(
+                            
+                                <Content 
+                                
+                                    key={"content" + (index + 1)}
+
+                                    author={post.author.username} 
+                                    date={post.dateEdited}
+
+                                    title={post.title}
+                                    body={post.body}
+
+                                />                           
+                            );
+                        })}
+
+                    </div>
+                    
+                    <div className="page-right-bottom page-section">
+
+                        <Users />                   
+                        <RightFooter />
+
+                    </div>
 
                 </div>
 
             </div>
+        );
+    }
 
-        </div>
-    );
+    else
+        return(
+        
+            <div id="feed-page" className="page">
+                
+                <div className="page-top page-section">
+                    <Header />
+                    
+                </div>
+
+                <div className="page-middle page-section">
+
+                    <div className="page-left-bottom page-section">
+
+                        <Sidebar />
+                        <LeftFooter />
+
+                    </div>
+
+                    <div id="content-card-container">Loading..</div>
+
+                    <div className="page-right-bottom page-section">
+
+                        <Users />                   
+                        <RightFooter />
+
+                    </div>
+
+                </div>
+
+            </div>
+        
+        );
 }
 
 export default Feed;
