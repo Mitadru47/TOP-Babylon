4498db159245f4634b507097dab3bbb5ce0b33db
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-02-06 14:07:21 +0530

    Header Component - Added Real-Time Clock

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,67 @@
+import { useEffect, useState } from "react";
+
 function Header(){
-    return(
+    
+    const [ timestamp, setTimestamp ] = useState();
 
-        <div id="header-component" className="component">
-            
-            <div id="site-title-container">
+    useEffect(() => {
 
-                <div id="site-title">Babylon</div>
+        const intervalID = setInterval(() => {
+            setTimestamp(() => new Date().toString());
+
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalID);
+        }
+
+    }, []);
+
+    if(timestamp){
+    
+        return(
+
+            <div id="header-component" className="component">
+                
+                <div id="site-title-container">
+
+                    <div id="site-title">Babylon</div>
+
+                </div>
+                
+                <div id="greeting-date-time-container">
+
+                    <div id="greeting">Hi User!</div>
+                    <div id="header-date-time">{timestamp}</div>
+
+                </div>
 
             </div>
-            
-            <div id="greeting-date-time-container">
+        );
+    }
 
-                <div id="greeting">Hi User!</div>
-                <div id="header-date-time">Tue Jan 28 2025 19:24:51 GMT+0530 (India Standard Time)</div>
+    else{
+
+        return(
+
+            <div id="header-component" className="component">
+                
+                <div id="site-title-container">
+
+                    <div id="site-title">Babylon</div>
+
+                </div>
+                
+                <div id="greeting-date-time-container">
+
+                    <div id="greeting">Hi User!</div>
+                    <div id="header-date-time">Loading..</div>
+
+                </div>
 
             </div>
-
-        </div>
-    );
+        );
+    }
 }
 
 export default Header;
