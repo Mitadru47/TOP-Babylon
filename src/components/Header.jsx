7af40c070450616aec8f98bd0610eae07dd2771d
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-02-19 21:32:21 +0530

    Header Component - Established Proper User Greeting & JWT Expiration ETA

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,67 +1,93 @@
+import axios from "../utils/axios";
 import { useEffect, useState } from "react";
+
+import { isLoggedIn, tokenExpiresIn } from "../utils/auth";
+import Login from "../components/Login";
+
+async function getUsername(setUsername){
+
+    axios.get("/users/" + localStorage.getItem("uid"))
+
+        .then((response) => setUsername(response.data))
+        .catch((error) => console.log(error));
+}
 
 function Header(){
     
-    const [ timestamp, setTimestamp ] = useState();
+    if(isLoggedIn()){
 
-    useEffect(() => {
+        const [ username, setUsername ] = useState();
+        const [ timestamp, setTimestamp ] = useState();
 
-        const intervalID = setInterval(() => {
-            setTimestamp(() => new Date().toString());
+        useEffect(() => {
 
-        }, 1000);
+            getUsername(setUsername);
 
-        return () => {
-            clearInterval(intervalID);
+            const intervalID = setInterval(() => {
+                setTimestamp(() => new Date().toUTCString());
+
+            }, 1000);
+
+            return () => {
+                clearInterval(intervalID);
+            }
+
+        }, []);
+
+        if(timestamp){
+            
+            return(
+
+                <div id="header-component" className="component">
+                    
+                    <div id="site-title-container">
+
+                        <div id="site-title">Babylon</div>
+
+                    </div>
+                    
+                    <div id="greeting-date-time-container">
+
+                        <div id="greeting">Hi { username ?  username : "User" }!</div>
+                        <div id="auth-token-expiration-eta">Your authentication token expires {tokenExpiresIn()}.</div>
+
+                        <div id="header-date-time">{timestamp}</div>
+
+                    </div>
+
+                </div>
+            );
+            
         }
 
-    }, []);
+        else{
 
-    if(timestamp){
-    
-        return(
+            return(
 
-            <div id="header-component" className="component">
-                
-                <div id="site-title-container">
+                <div id="header-component" className="component">
+                    
+                    <div id="site-title-container">
 
-                    <div id="site-title">Babylon</div>
+                        <div id="site-title">Babylon</div>
+
+                    </div>
+                    
+                    <div id="greeting-date-time-container">
+
+                        <div id="greeting">Hi User!</div>
+                        <div id="auth-token-expiration-eta">Your authentication token expires in..</div>
+
+                        <div id="header-date-time">Loading..</div>
+
+                    </div>
 
                 </div>
-                
-                <div id="greeting-date-time-container">
-
-                    <div id="greeting">Hi User!</div>
-                    <div id="header-date-time">{timestamp}</div>
-
-                </div>
-
-            </div>
-        );
+            );
+        }
     }
 
-    else{
-
-        return(
-
-            <div id="header-component" className="component">
-                
-                <div id="site-title-container">
-
-                    <div id="site-title">Babylon</div>
-
-                </div>
-                
-                <div id="greeting-date-time-container">
-
-                    <div id="greeting">Hi User!</div>
-                    <div id="header-date-time">Loading..</div>
-
-                </div>
-
-            </div>
-        );
-    }
+    else
+        return <Login />;
 }
 
 export default Header;
