b4ef665b42cd84170f283c150f28c270844562e1
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-03-21 23:50:27 +0530

    Users Component - Infinite Scroll Implementation

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,49 +1,118 @@
+import { isLoggedIn } from "../utils/auth";
 import axios from "../utils/axios";
+
 import { useState, useEffect } from "react";
+import Login from "./Login";
+
+const usernamesPerPage = 20;
+
+let totalUserCount = 0, pageNumber = 1, activeTimeoutFlag = false;
+let globalUsernameSet = new Set();
+
+async function getTotalUserCount(){
+
+    axios.get("/users/count")
+
+        .then((response) => totalUserCount = response.data)
+        .catch((error) => console.log(error));
+}
 
 async function getUserList(setUserList, setLoaderStatus){
 
-    axios.get("/users")
+    const requestBody = JSON.stringify(
+        { usernamesPerPage: usernamesPerPage, pageNumber: pageNumber });
+
+    axios.post("/users", requestBody)
 
         .then((response) => {
         
-            setUserList(response.data)
+            response.data.forEach((username) => {
+
+                if(!globalUsernameSet.has(username))
+                    globalUsernameSet.add(username);
+            });
+
+            setUserList({ usernames: Array.from(globalUsernameSet) })
             setLoaderStatus(false);
         })
+        
         .catch((error) => console.log(error));
+}
+
+function infiniteScroll(setUserList, setLoaderStatus){
+
+    const userListContainer = document.querySelector("#user-list-container");
+    const userListItems = document.querySelectorAll(".user-list-item");
+
+    if(userListContainer.clientHeight + userListContainer.scrollTop >= userListContainer.scrollHeight - 1){
+
+        if(userListItems.length === totalUserCount)
+            setLoaderStatus(false);
+
+        else if(!activeTimeoutFlag){
+
+            setLoaderStatus(true);
+            activeTimeoutFlag = true;
+
+            setTimeout(() => {
+
+                pageNumber++;
+                activeTimeoutFlag = true;
+
+                getUserList(setUserList, setLoaderStatus);
+
+            }, 2500);
+        }
+    }
 }
 
 function Users(){
 
-    const [userList, setUserList] = useState();
-    const [loaderStatus, setLoaderStatus] = useState(true);
-    
-    useEffect(() => {
-        getUserList(setUserList, setLoaderStatus);
+    if(isLoggedIn()){
 
-    }, []);
+        const [userList, setUserList] = useState();
+        const [loaderStatus, setLoaderStatus] = useState(true);
+        
+        useEffect(() => {
 
-    return(
+            if(pageNumber === 1){
 
-        <div id="users-component" className="component">
-            
-            <div id="user-list-header-container">
-            
-                <div id="user-list-header">Fellow Babylonians</div>
+                getTotalUserCount();
+                getUserList(setUserList, setLoaderStatus);
+            }
+
+            const userListContainer = document.querySelector("#user-list-container");
+            userListContainer.addEventListener("scroll", () => infiniteScroll(setUserList, setLoaderStatus));
+
+            return () => userListContainer.removeEventListener("scroll", () => infiniteScroll(setUserList, setLoaderStatus));
+
+        }, []);
+
+        return(
+
+            <div id="users-component" className="component">
                 
-            </div>
-
-            <div id="user-list-container">
+                <div id="user-list-header-container">
                 
-                { userList && userList.map((user, index) => {
-                    return(<div key={"user" + (index + 1)} className="user-list">{user}</div>); })}
+                    <div id="user-list-header">Fellow Babylonians</div>
+                    
+                </div>
 
-                { loaderStatus && "Loading.." }
+                <div id="user-list-container">
+                    
+                    { userList && userList.usernames.map((user, index) => {
+                        return(<div key={"user" + (index + 1)} className="user-list-item">{user}</div>); })}
+
+                    { loaderStatus && "..." }
+
+                </div>
 
             </div>
+        );
+    }
 
-        </div>
-    );
+    else
+        return <Login />;
 }
 
 export default Users;
