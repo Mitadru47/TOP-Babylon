86512dd34207460b5f766813a3c69f70c944790b
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2025-03-25 21:54:23 +0530

    Header Component - Small Optimization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,57 +35,28 @@ function Header(){
             }
 
         }, []);
-
-        if(timestamp){
             
-            return(
+        return(
 
-                <div id="header-component" className="component">
-                    
-                    <div id="site-title-container">
+            <div id="header-component" className="component">
+                
+                <div id="site-title-container">
 
-                        <div id="site-title">Babylon</div>
-
-                    </div>
-                    
-                    <div id="greeting-date-time-container">
-
-                        <div id="greeting">Hi { username ?  username : "User" }!</div>
-                        <div id="auth-token-expiration-eta">Your authentication token expires {tokenExpiresIn()}.</div>
-
-                        <div id="header-date-time">{timestamp}</div>
-
-                    </div>
+                    <div id="site-title">Babylon</div>
 
                 </div>
-            );
-            
-        }
+                
+                <div id="greeting-date-time-container">
 
-        else{
+                    <div id="greeting">Hi { username ?  username : "User" }!</div>
+                    <div id="auth-token-expiration-eta">Your authentication token expires {tokenExpiresIn() ? tokenExpiresIn() : "in.."}.</div>
 
-            return(
-
-                <div id="header-component" className="component">
-                    
-                    <div id="site-title-container">
-
-                        <div id="site-title">Babylon</div>
-
-                    </div>
-                    
-                    <div id="greeting-date-time-container">
-
-                        <div id="greeting">Hi User!</div>
-                        <div id="auth-token-expiration-eta">Your authentication token expires in..</div>
-
-                        <div id="header-date-time">Loading..</div>
-
-                    </div>
+                    <div id="header-date-time">{timestamp ? timestamp : "Loading.."}</div>
 
                 </div>
-            );
-        }
+
+            </div>
+        );
     }
 
     else
